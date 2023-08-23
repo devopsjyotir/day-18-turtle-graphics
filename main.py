75c8367c80a177ac72fc1fdb9964cc986e5d6ea9
devopsjyotir <jyotirjy@gmail.com>
2023-08-23 16:09:52 -0400

    created loop to draw shapes incrementing number of sides

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-hello
+from turtle import Turtle, Screen
+
+import heroes
+
+print(heroes.gen())
+# import turtle as t
+amzy = Turtle()
+
+# for i in range(4):
+#     amzy.forward(100)
+#     amzy.right(90)
+# for i in range(20):
+#     amzy.forward(10)
+#     amzy.penup()
+#     amzy.forward(10)
+#     amzy.pendown()
+
+num_of_sides = 3
+
+while num_of_sides < 11:
+    for j in range(num_of_sides):
+        right_angle = 360 / num_of_sides
+        amzy.forward(100)
+        amzy.right(right_angle)
+    num_of_sides += 1
+
+screen = Screen()
+screen.exitonclick()
