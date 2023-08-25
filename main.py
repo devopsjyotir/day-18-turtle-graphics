cfd0117ac36cbf1664f7d36d3447cd6ea4498157
devopsjyotir <jyotirjy@gmail.com>
2023-08-25 05:18:21 -0400

    changed turtle speed and created random walk

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 from turtle import Turtle, Screen
-
-import heroes
-
-print(heroes.gen())
+import random
 # import turtle as t
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270, 360]
 amzy = Turtle()
+# num_of_sides = 3
+#
+# while num_of_sides < 11:
+#     amzy.pencolor(random.choice(colors))
+#     for j in range(num_of_sides):
+#         right_angle = 360 / num_of_sides
+#         amzy.forward(100)
+#         amzy.right(right_angle)
+#     num_of_sides += 1
 
-# for i in range(4):
-#     amzy.forward(100)
-#     amzy.right(90)
-# for i in range(20):
-#     amzy.forward(10)
-#     amzy.penup()
-#     amzy.forward(10)
-#     amzy.pendown()
+coin_flip = ['heads', 'tails']
+i = 0
+while i < 100:
+    amzy.speed(10)
+    amzy.pensize(10)
+    amzy.hideturtle()
+    amzy.pencolor(random.choice(colors))
+    amzy.forward(20)
+    amzy.setheading(random.choice(directions))
+    i += 1
 
-num_of_sides = 3
 
-while num_of_sides < 11:
-    for j in range(num_of_sides):
-        right_angle = 360 / num_of_sides
-        amzy.forward(100)
-        amzy.right(right_angle)
-    num_of_sides += 1
+
+
+
+
+
+
+
+
+
+
+
+
 
 screen = Screen()
 screen.exitonclick()
